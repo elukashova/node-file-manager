b509a04a5aa1f7a003524bf7f74928578c36d0c2
elukashova <ele.lukashova@gmail.com>
2024-02-05 18:52:40 +0100

    feat: implement cp operation

--- src/file-manager/commands/rn/rn.js
+++ src/file-manager/commands/rn/rn.js
@@ -5,6 +5,10 @@ import { doesFileExist } from '../../../utils/doesFileExist.js';
 import { ERROR_MESSAGES } from '../../../consts.js';
 
 export default async (args) => {
+    if (!args.length) {
+        throw Error(ERROR_MESSAGES.invalidInput);
+    }
+    
     const [ oldName, newName ] = args;
     const oldFilePath = path.resolve(cwd(), oldName);
     const newFilePath = path.resolve(path.dirname(oldFilePath), newName);
