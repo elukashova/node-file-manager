7f63319c4e7ec5a91ed21217758dcfe636d13565
elukashova <ele.lukashova@gmail.com>
2024-02-05 17:17:34 +0100

    feat: implement rn operation

---
+++ src/file-manager/commands/rn/rn.js
@@ -0,0 +1,17 @@
+import { rename } from 'node:fs/promises';
+import * as path from 'node:path';
+import { cwd } from 'node:process';
+import { doesFileExist } from '../../../utils/doesFileExist.js';
+import { ERROR_MESSAGES } from '../../../consts.js';
+
+export default async (args) => {
+    const [ oldName, newName ] = args;
+    const oldFilePath = path.resolve(cwd(), oldName);
+    const newFilePath = path.resolve(path.dirname(oldFilePath), newName);
+
+    if (!await doesFileExist(oldFilePath) || await doesFileExist(newFilePath)) {
+        throw Error(ERROR_MESSAGES.operationFailed);
+    }
+
+    await rename(oldFilePath, newFilePath);
+}
