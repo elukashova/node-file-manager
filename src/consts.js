28d2d2b080b5aa3c831c8f172047844c690c85e0
elukashova <ele.lukashova@gmail.com>
2024-02-05 11:28:53 +0100

    feat: implement process start and end

---
+++ src/consts.js
@@ -0,0 +1,4 @@
+export const ERROR_MESSAGES = {
+    invalidInput: 'Invalid input',
+    operationFailed: 'Operation failed',
+}
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { FileManager } from './file-manager.js';
+
+class App {
+    constructor() {
+        const args = process.argv.slice(2);
+        const username = this.getUsername(args);
+        const fileManager = new FileManager(username);
+        fileManager.init();
+    }
+
+    getUsername(args) {
+        const usernameArg = args.find((arg) => arg.startsWith('--username'));
+
+        if (!usernameArg) {
+            console.log('No username has been provided.');
+            return 'Anonymous';
+        }
+
+        return usernameArg.split('=')[1];
+    }
+}
+
+new App();
